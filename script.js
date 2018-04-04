cff6ac777bdd8687d99c411211414dae697bbf25
Marina Valiquette <marina.valiquette@gmail.com>
2018-04-04 15:51:09 -0400

    Implement factory function with LOTR

--- script.js
+++ script.js
@@ -78,7 +78,77 @@ function decodeWords (str) {
     .join('');
 }
 
-console.log(decodeWords('craft block argon meter bells brown croon droop'));
+// console.log(decodeWords('craft block argon meter bells brown croon droop'));
+
+function createCharacter(name, nickName, race, origin, attack, defense) {
+  // this.name = name;
+  return {
+    name,
+    nickName,
+    race,
+    origin,
+    attack,
+    defense,
+    describe() {
+      console.log(`${this.name} is a ${this.race} from ${this.origin}.`);
+    },
+    evaluateFight(obj) {
+      console.log(
+        `Your opponent takes ${(this.attack - obj.defense < 0) ? 0 : this.attack - obj.defense} damage and you receive ${(obj.attack - this.defense < 0 ? 0 : obj.attack - this.defense)} damage.`)
+    },
+  };
+}
+
+const characters = [
+  createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6),
+  createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1),
+  createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2),
+  createCharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 8),
+  createCharacter('Legolas', 'legolas', 'Elf', 'Woodland Realm', 8, 5),
+  createCharacter('Arwen Undomiel', 'arwen', 'Half-Elf', 'Rivendell', 9, 3),
+]
+
+// Use .find() to retrieve character with nickname 'aragorn' and then call its describe method
+// console.log(characters.find(obj => obj.nickName === 'aragorn').describe());
+
+// Use .filter() to print a new array from characters that ONLY contains characters of the race Hobbit
+// console.log(characters.filter(obj => obj.race === 'Hobbit'));
+
+// Use .filter() to print a new array from characters that ONLY contains characters with attack value above 5
+// console.log(characters.filter(obj => obj.attack > 5));
+
+// Add property 'weapon' to each character and modify describe()
+characters.forEach(obj => {
+  if(obj.nickName === 'gandalf') obj.weapon = 'Wizard staff';
+  if(obj.nickName === 'bilbo') obj.weapon = 'Ring';
+  if(obj.nickName === 'frodo') obj.weapon = 'String and Barrow Blade';
+  if(obj.nickName === 'aragorn') obj.weapon = 'Anduril';
+  if(obj.nickName === 'legolas') obj.weapon = 'Bow and Arrow';
+  if(obj.nickName === 'arwen') obj.weapon = 'Bow and Arrow';
+  obj.describe = function() {console.log(`${this.name} is a ${this.race} of ${this.origin} who uses ${this.weapon}.`)};
+})
+
+// console.log(characters[0].describe());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
